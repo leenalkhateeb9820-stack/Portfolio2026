df3ccc02b9e60e95603e21c6933cae66c3508891
leenalkhateeb9820-stack <leenalkhateeb9820@gmail.com>
2026-01-20 18:23:47 +0300

    Update server.js
    cleanup

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-
-// يخبر السيرفر أن يقرأ الصور والملفات من المجلد الحالي
 app.use(express.static(__dirname)); 
 
 const mongoURI = process.env.MONGODB_URI;
@@ -27,10 +26,8 @@ const projectSchema = new mongoose.Schema({
 
 const Project = mongoose.model('Project', projectSchema);
 
-// --- [إضافة: بوابة الأمان للتحقق من الباسورد] ---
 app.post('/api/verify-password', (req, res) => {
     const { password } = req.body;
-    // الباسورد سيتم سحبه من إعدادات Render لاحقاً
     const securePassword = process.env.ADMIN_PASSWORD; 
 
     if (password === securePassword) {
@@ -39,11 +36,9 @@ app.post('/api/verify-password', (req, res) => {
         res.status(401).json({ success: false, message: "Incorrect Password!" });
     }
 });
-// ------------------------------------------
 
-// مسار الصفحة الرئيسية
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.get('/api/projects', async (req, res) => {
