f7987ff86ab065221c5cb9fc44b5450907800623
leenalkhateeb9820-stack <leenalkhateeb9820@gmail.com>
2026-01-20 18:22:22 +0300

    Update projects.js
    cleanup

--- projects.js
+++ projects.js
@@ -4,12 +4,11 @@ const projectsData = [
         type: "E-Commerce Solution",
         description: "A comprehensive digital bakery experience where I engineered a seamless ordering system and high-conversion UI.",
         tags: ["Full Stack Architecture", "Tailwind CSS", "UX Design"],
-        link: "https://croissantcobakery.netlify.app", // يمكنك تحديثه برابط Render الجديد لاحقاً
+        link: "https://croissant-co-bakery.onrender.com",
         image: "croissant.webp",
-        colorClass: "text-orange-500", // هذا اللي بحدد لون النص
-        glowClass: "project-glow"      // هذا اللي بحدد لون الوميض (Glow)
-    },
-    // لما تخلصي مشروع جديد، بس انسخي اللي فوق وحطيه هنا بنفس الأسماء
+        colorClass: "text-orange-500",
+        glowClass: "project-glow"
+    }
 ];
 
 export default projectsData;
